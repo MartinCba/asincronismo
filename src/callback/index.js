7448fed89a1f1a2fc96086b3a918b8b971b0ecba
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2021-11-10 17:56:44 -0300

    commit 1 callback

---
+++ src/callback/index.js
@@ -0,0 +1,27 @@
+//---------------CALLBACK-------------------------//
+
+function suma(num1, num2) {
+  return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+console.log(calc(6, 2, suma));
+
+//-------------------------------------------------//
+
+function date(callback) {
+  console.log(new Date());
+  setTimeout(function () {
+    let date = new Date();
+    callback(date);
+  }, 3000);
+}
+
+function printDate(dateNow) {
+  console.log(dateNow);
+}
+
+date(printDate);
